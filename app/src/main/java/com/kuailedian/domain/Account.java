0648c5c1d5b2e08d7206b508efe6c4400a684364
maxzhang1985 <maxzhang1985@gmail.com>
2015-04-09 17:10:48 +0800

    添加Domain层，并添加账户领域模型

---
+++ app/src/main/java/com/kuailedian/domain/Account.java
@@ -0,0 +1,45 @@
+package com.kuailedian.domain;
+
+/**
+ * Created by maxzhang on 4/9/2015.
+ */
+public class Account {
+
+    private String name;
+    private int age;
+    private String mobilePhone;
+    private boolean lockChecked ;
+
+    public Account(String phone)
+    {
+        this.mobilePhone = phone;
+        lockChecked = false;
+    }
+
+
+
+
+    public void CheckIn()
+    {
+        if(!lockChecked)
+        {
+            //
+
+
+        }
+
+
+    }
+
+
+    public boolean Register()
+    {
+
+        return false;
+    }
+
+
+
+
+
+}
