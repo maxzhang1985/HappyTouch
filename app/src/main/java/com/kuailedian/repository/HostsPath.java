6f770964546d194873995cf1951a5baeb7d5f134
maxzhang1985 <maxzhang1985@gmail.com>
2015-07-01 13:08:15 +0800

    新图标

--- app/src/main/java/com/kuailedian/repository/HostsPath.java
+++ app/src/main/java/com/kuailedian/repository/HostsPath.java
@@ -4,7 +4,7 @@ package com.kuailedian.repository;
  * Created by maxzhang on 6/1/2015.
  */
 public class HostsPath {
-    public static String HostUri = "http://220.194.201.2:21121/jiekou/";
+    public static String HostUri = "http://220.194.201.2:21121/test/jiekou/";
     //public static String HostUri = "http://60.2.176.70:21121/test/jiekou/";
 
 }
